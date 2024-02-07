2b27299ed5c7f5f96dd25e6fe7100da635a62461
HamzaDogann <hamzadgn011@gmail.com>
2024-02-07 20:36:14 +0300

    first commit

---
+++ src/veriler.jsx
@@ -0,0 +1,13 @@
+function veriler() {
+    return [
+      { id: 0, title: 'Telefon 1', done: true },
+      { id: 1, title: 'Telefon 2', done: false },
+      { id: 2, title: 'Laptop 1', done: false },
+      { id: 3, title: 'Laptop 2', done: false },
+      { id: 4, title: 'Yazıcı 1', done: false },
+      { id: 5, title: 'Yazıcı 2', done: false }
+    ];
+  }
+  
+  export default veriler;
+  
